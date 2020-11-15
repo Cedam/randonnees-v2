caa018382bdafc3e428427b07b705649d6a5f67c
Cedam <cedam56@gmail.com>
2020-11-15 20:46:30 +0100

    Add MainServiceTest

---
+++ src/test/java/org/cedam/application/randonnees/main/MainServiceTest.java
@@ -0,0 +1,25 @@
+package org.cedam.application.randonnees.main;
+
+import org.cedam.application.randonnees.appconfig.AppConfigService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = AppConfigService.class)
+@SpringBootTest
+public class MainServiceTest {
+
+		
+		@Test
+		@Transactional
+		public void testGetById() {
+			MainService.main(null);
+		}
+		
+		
+}
