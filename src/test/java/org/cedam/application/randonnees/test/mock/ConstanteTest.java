f0f28affcfa599642eaf37e011bcdc9a44287c77
Cedam <cedam56@gmail.com>
2021-01-24 14:18:59 +0100

    test

---
+++ src/test/java/org/cedam/application/randonnees/test/mock/ConstanteTest.java
@@ -0,0 +1,61 @@
+package org.cedam.application.randonnees.test.mock;
+
+import org.cedam.application.randonnees.dto.DayDto;
+import org.cedam.application.randonnees.dto.TrekDto;
+import org.cedam.application.randonnees.entity.Day;
+import org.cedam.application.randonnees.entity.Trek;
+
+public class ConstanteTest {
+
+	public static Trek getTrek() {
+		var trek = new Trek();
+		trek.setId(TREK_TEST_ID_1);
+		trek.setName(TREK_TEST_NAME_1);
+		trek.setLocation(TREK_TEST_LOCATION_1);
+		return trek;
+	}
+
+	public static TrekDto getTrekDto() {
+		var trekDto = new TrekDto();
+		trekDto.setId(TREK_TEST_ID_1);
+		trekDto.setName(TREK_TEST_NAME_1);
+		trekDto.setLocation(TREK_TEST_LOCATION_1);
+		return trekDto;
+	}
+	
+	public static Trek getTrek2() {
+		var trek = new Trek();
+		trek.setId(TREK_TEST_ID_2);
+		trek.setName(TREK_TEST_NAME_2);
+		trek.setLocation(TREK_TEST_LOCATION_2);
+		return trek;
+	}
+
+	public static Day getDay() {
+		var day = new Day();
+		day.setNumber(DAY_TEST_NUMBER_1);
+		day.setTrek(getTrek());
+		return day;
+	}
+
+	public static DayDto getDayDto() {
+		var dayDto = new DayDto();
+		dayDto.setNumber(DAY_TEST_NUMBER_1);
+		dayDto.setTrek(getTrekDto());
+		return dayDto;
+	}
+
+	
+	public final static Long DAY_TEST_ID_1 = 1L;
+	public static final Long DAY_TEST_ID_2 = 2L;
+	public final static String DAY_TEST_NUMBER_1 = "TEST_NUMBER_1";
+
+	public final static Long TREK_TEST_ID_1 = 1L;
+	public final static String TREK_TEST_NAME_1 = "TEST_NAME_1";
+	public final static String TREK_TEST_LOCATION_1 = "TEST_NUMBER_1";
+
+	public final static Long TREK_TEST_ID_2 = 2L;
+	public final static String TREK_TEST_NAME_2 = "TEST_NUMBER_2";
+	public final static String TREK_TEST_LOCATION_2 = "TEST_NUMBER_2";
+
+}
