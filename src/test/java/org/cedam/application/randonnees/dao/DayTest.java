fe173ee1a15ce4510a986ca8d85b4ba5aed60d79
Cedam <cedam56@gmail.com>
2021-01-24 11:28:16 +0100

    Roolback building version

---
+++ src/test/java/org/cedam/application/randonnees/dao/DayTest.java
@@ -0,0 +1,69 @@
+package org.cedam.application.randonnees.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.cedam.application.randonnees.appconfig.AppConfigDao;
+import org.cedam.application.randonnees.entity.Day;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = AppConfigDao.class)
+@SpringBootTest
+public class DayTest {
+
+	@Autowired
+	private DayDao object;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	@Transactional
+	public void SaveTest() {
+		Day dayResult = object.save(ConstanteTest.getDay());
+		Assert.assertTrue(dayResult != null);
+		Assert.assertTrue(dayResult.getId() > 0);
+	}
+
+	@Test
+	@Transactional
+	public void FindTest() {
+
+		// Find a Day by ID
+		Optional<Day> result = object.findById(ConstanteTest.DAY_TEST_ID_1);
+		Assert.assertTrue(!result.isEmpty());
+		Assert.assertTrue(result.isPresent());
+
+		// Find Day by last number
+		List<Day> result2 = object.findByNumber(ConstanteTest.DAY_TEST_NUMBER_1);
+		Assert.assertTrue(!result2.isEmpty());
+		Assert.assertTrue(result2.size() > 0);
+
+	}
+
+}
