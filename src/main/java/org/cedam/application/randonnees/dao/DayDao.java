51b102024a98cd2481502c1a23a42fa2e37e7df5
Cedam <cedam56@gmail.com>
2021-03-04 16:20:07 +0100

    Change SQL request

--- src/main/java/org/cedam/application/randonnees/dao/DayDao.java
+++ src/main/java/org/cedam/application/randonnees/dao/DayDao.java
@@ -7,15 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface DayDao extends JpaRepository<Day, Long> {
+public interface DayDao extends JpaRepository<Day, Long>  {
 	
-	public static final String RQT_FIND_BY_TREK_ID = "SELECT ID, NUMBER, TREK_ID FROM Day p WHERE p.TREK_ID = :trekId";
 	
 	List<Day> findByNumber(String number);
 
 	List<Day> findListByTrekId(Long id);
 	
-	@Query(value = RQT_FIND_BY_TREK_ID, nativeQuery=true)
+	@Query(value = "SELECT ID, NUMBER, TREK_ID FROM Day p WHERE p.TREK_ID = :trekId", nativeQuery=true)
 	List<Day> findListByTrekIdSql(@Param("trekId") Long id);
 
 
